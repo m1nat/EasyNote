9a2744f78951a78744252d66b6a6db30811198b6
Vitaliy <Mushpakov.Vitaliy@gmail.com>
2021-07-22 01:49:51 +0300

    signIn Layout

--- src/shared/validators/regexp.js
+++ src/shared/validators/regexp.js
@@ -4,7 +4,7 @@ export const REGEXP = {
   LOWER_CASE: /(?=.*[a-z])/g,
   NUMBERS: /(?=.*[0-9])/gi,
   EIGHT_CHARACTERS: /(?=.*[!@#$%^&*])/gi,
-  SHOW_ERROR_MESSEGE_PASSWORD: /^[a-zA-Z0-9@()=+!^%$#&_-]{8,20}$/gi,
+  SHOW_ERROR_MESSEGE_PASSWORD: /^[a-zA-Z0-9@()=+!^%$#&_-]{6,20}$/gi,
   USERNAME: /^[a-zA-Z0-9]{6,20}$/
 };
 
