709e0779555b5e1ec457a2c05ea7965985e4f64d
Vitaliy <Mushpakov.Vitaliy@gmail.com>
2021-07-23 00:56:32 +0300

    demo-page

--- src/shared/constants/paths.js
+++ src/shared/constants/paths.js
@@ -1,11 +1,13 @@
 export const routs = {
   main: '/',
   sign_in: 'sign-in.html',
-  sign_up: 'sign-up.html'
+  sign_up: 'sign-up.html',
+  start_page: 'start-page.html'
 };
 
 export const paths = {
   main: '/',
   sign_in: '/sign-in.html',
-  sign_up: '/sign-up.html'
+  sign_up: '/sign-up.html',
+  start_page: '/start-page.html'
 };
