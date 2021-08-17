071edc2809e89378bbd0889fffb70c63ac29a96f
Vitaliy <Mushpakov.Vitaliy@gmail.com>
2021-08-17 22:52:09 +0300

    test2

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle[chunkhash].js',
+    filename: 'bundle.[chunkhash].js',
     path: path.resolve(__dirname, 'build')
   },
   plugins: [
