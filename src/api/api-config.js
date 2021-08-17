c6abd5c3fdd901903daa5267f57d9f06eadfc87c
Vitaliy <Mushpakov.Vitaliy@gmail.com>
2021-08-17 22:29:25 +0300

    dep3

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -8,6 +8,7 @@ export const FIREBASE_CONFIG = {
 };
 
 export const databaseURL = 'https://minilab-badae-default-rtdb.firebaseio.com';
+
 export const authURL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_CONFIG.apiKey}`;
 
 
