7243cc2c65ac0498cdd7b9f9d57f4ba7c08c9fb5
Vitaliy <Mushpakov.Vitaliy@gmail.com>
2021-08-17 21:53:10 +0300

    dep1

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+
 module.exports = {
   entry: './src/index.js',
   output: {
